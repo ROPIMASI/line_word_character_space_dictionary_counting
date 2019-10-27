00e9547ecbd2e730e494620b08b5d95a40c1c90a
ROPIMASI <ropimasi@email.com>
2019-10-27 16:28:39 -0300

    revisao rotina

---
+++ copy_shortening_name.bat
@@ -0,0 +1,7 @@
+@echo on
+ 
+echo copy_shortening_name
+ 
+copy line_word_character_space_dictionary_counting.py lwcsdcounting.py
+ 
+pause
--- reduce_name_copying.bat
+++
@@ -1,2 +0,0 @@
-copy line_word_character_space_dictionary_counting.py lwcsdcounting
-copy line_word_character_space_dictionary_counting.py text_file_detailer
